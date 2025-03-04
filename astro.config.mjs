60dec20cd639501d077ebcab505c6ccae91916f1
rrichardson <rr.designweb@gmail.com>
2025-03-04 13:28:04 +1100

    Adding sitemaps and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://rasjahknow.com",
+  integrations: [react(), tailwind(), sitemap()]
 });
