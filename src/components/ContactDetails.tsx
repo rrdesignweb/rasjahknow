a8f784f85e4c19e9afa5c0b340f3c8243458af61
rrichardson <rr.designweb@gmail.com>
2026-03-29 19:23:48 +1100

    fix(styles): spacing and fonts sizing

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -1,36 +1,49 @@
 const ContactDetails = () => {
   return (
-    <p className="!mb-0 md:text-right md:pt-0 pt-4">
-      <strong>Manager - Jorge Abreu (Worldwide)</strong> <br />
-      Phone:{" "}
-      <a
-        className="text-red-700 hover:text-red-900"
-        href="tel:+61 416 652 906"
-        data-obfuscation="0"
-      >
-        +61 416 652 906
-      </a>
-      <br />
-      Email:{" "}
-      <a
-        className="text-red-700 hover:text-red-900"
-        href="mailto:rasjahknowband@gmail.com"
-        data-obfuscation="0"
-      >
-        rasjahknowband@gmail.com
-      </a>
-      <br />
-      <br />
-      <strong>Bookings Management (Worldwide)</strong> <br />
-      Email:{" "}
-      <a
-        className="text-red-700 hover:text-red-900"
-        href="mailto:bookings@rasjahknow.com"
-        data-obfuscation="0"
-      >
-        bookings@rasjahknow.com
-      </a>
-    </p>
+    <div className="flex flex-col gap-4 pt-0  md:justify-end md:gap-6 md:text-right">
+       <div>
+        <p className="!mb-1">
+          <strong>Bookings Management (Worldwide)</strong>
+        </p>
+        <p className="!mb-0">
+          Email:{" "}
+          <a
+            className="text-red-700 hover:text-red-900"
+            href="mailto:bookings@rasjahknow.com"
+            data-obfuscation="0"
+          >
+            bookings@rasjahknow.com
+          </a>
+        </p>
+      </div>
+      <div>
+        <p className="!mb-1">
+          <strong>Manager - Jorge Abreu (Worldwide)</strong>
+        </p>
+        <p className="!mb-1">
+          Phone:{" "}
+          <a
+            className="text-red-700 hover:text-red-900"
+            href="tel:+61 416 652 906"
+            data-obfuscation="0"
+          >
+            +61 416 652 906
+          </a>
+        </p>
+        <p className="!mb-0">
+          Email:{" "}
+          <a
+            className="text-red-700 hover:text-red-900"
+            href="mailto:rasjahknowband@gmail.com"
+            data-obfuscation="0"
+          >
+            rasjahknowband@gmail.com
+          </a>
+        </p>
+      </div>
+
+     
+    </div>
   );
 };
 
